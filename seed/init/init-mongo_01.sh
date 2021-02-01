3cda390a3f69de2dc482aba21f2533c47edb73bd
ranajit007 <50741727+ranajit007@users.noreply.github.com>
2021-02-01 18:05:10 +0530

    Update init-mongo_01.sh

--- seed/init/init-mongo_01.sh
+++ seed/init/init-mongo_01.sh
@@ -6,5 +6,5 @@ echo "      1. Importing seed data...";
 for f in /seed/files/* 
 do
     echo "      > Seed: $f";
-    mongoimport -d=$MONGO_INITDB_DATABASE -u=incidentUser -p=2020*Secret --file=$f --jsonArray
+    mongoimport -d=$MONGO_INITDB_DATABASE -u=incidentUser -p=redhat --file=$f --jsonArray
 done
